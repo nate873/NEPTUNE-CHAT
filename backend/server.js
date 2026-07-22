c850c11ef5be98561d75265f8d54e08825766643
Nate Waldstein <nate@actionfunding.net>
2026-07-21 20:14:04 -0400

    changes

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,16 @@ app.use(cors());
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: { origin: "http://localhost:5174", methods: ["GET", "POST"] },
+  cors: {
+    origin: [
+      "https://neptunechat.app",
+      "https://www.neptunechat.app",
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:5175",
+    ],
+    methods: ["GET", "POST"],
+  },
 });
 
 // Users waiting to be matched
